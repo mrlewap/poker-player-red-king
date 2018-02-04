88f09d1a58e73ab8751a7b0d1fc0957251b3920e
Malyshkov Roman <malyshkov.roman@gmail.com>
2018-02-04 13:28:15 +0600

    BUG FIXES

--- player.py
+++ player.py
@@ -1,19 +1,15 @@
 from hand import Hand
 
 class Player:
-    VERSION = "Red King 0.1"
+    VERSION = "Red King 0.2"
 
     def betRequest(self, game_state):
-        try:
-            player_index = game_state['in_action']
+         player_index = game_state['in_action']
 
-            hand = Hand()
-            hand.setHand(game_state['players'][player_index]['hole_cards'])
+         hand = Hand()
+         hand.setHand(game_state['players'][player_index]['hole_cards'])
 
-            return hand.resolveStrategy(game_state)
-
-        except BaseException:
-            return game_state['players'][game_state['in_action']]['stack']
+         return hand.resolveStrategy(game_state)
 
     def showdown(self, game_state):
         pass
