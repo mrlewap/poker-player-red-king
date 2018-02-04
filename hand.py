3c375413e3d5f33e7317b85c9ba7051b617e9016
Malyshkov Roman <malyshkov.roman@gmail.com>
2018-02-04 13:53:19 +0600

    Fixes to hand.py

--- hand.py
+++ hand.py
@@ -3,18 +3,19 @@ from card import Card
 class Hand:
     def __init__(self):
         self.cards_in_hand = []
-        pass
 
     def setHand(self, hand):
         for each in hand:
-            card = Card(each.rank, each.suit)
+            card = Card(each['rank'], each['suit'])
             self.cards_in_hand.append(card)
 
+
     def resolveStrategy(self, game_state):
-        if self.resolveValue(self.cards_in_hand[0].rank) == self.resolveValue(self.cards_in_hand[1].rank):
+        if self.resolveValue(self.cards_in_hand[0]['rank']) == self.resolveValue(self.cards_in_hand[1]['rank']):
             return game_state['players'][game_state['in_action']]['stack']
 
-        if self.resolveValue(self.cards_in_hand[0].rank) >= 10 and self.resolveValue(self.cards_in_hand[1].rank) >= 10:
+        if self.resolveValue(self.cards_in_hand[0]['rank']) >= 10 and \
+                self.resolveValue(self.cards_in_hand[1]['rank']) >= 10:
             return game_state['players'][game_state['in_action']]['stack']
 
         return 0
