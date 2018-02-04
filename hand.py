ab97d825f5dca7e3c01f13ce975da9835f03b58a
Malyshkov Roman <malyshkov.roman@gmail.com>
2018-02-04 13:58:27 +0600

    Bug fixes

--- hand.py
+++ hand.py
@@ -11,11 +11,11 @@ class Hand:
 
 
     def resolveStrategy(self, game_state):
-        if self.resolveValue(self.cards_in_hand[0]['rank']) == self.resolveValue(self.cards_in_hand[1]['rank']):
+        if self.resolveValue(self.cards_in_hand[0].rank) == self.resolveValue(self.cards_in_hand[1].rank):
             return game_state['players'][game_state['in_action']]['stack']
 
-        if self.resolveValue(self.cards_in_hand[0]['rank']) >= 10 and \
-                self.resolveValue(self.cards_in_hand[1]['rank']) >= 10:
+        if self.resolveValue(self.cards_in_hand[0].rank) >= 10 and \
+                self.resolveValue(self.cards_in_hand[1].rank) >= 10:
             return game_state['players'][game_state['in_action']]['stack']
 
         return 0
