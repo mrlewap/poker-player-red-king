b1fe85e4b90346bf6c50f8fe1dce232b789fd0b8
Malyshkov Roman <malyshkov.roman@gmail.com>
2018-02-04 13:59:29 +0600

    Added error handling

--- player.py
+++ player.py
@@ -4,12 +4,16 @@ class Player:
     VERSION = "Red King 0.2"
 
     def betRequest(self, game_state):
+        try:
          player_index = game_state['in_action']
 
          hand = Hand()
          hand.setHand(game_state['players'][player_index]['hole_cards'])
 
          return hand.resolveStrategy(game_state)
+        except BaseException as ex:
+            print(ex)
+            return game_state['players'][game_state['in_action']]['stack']
 
     def showdown(self, game_state):
         pass
