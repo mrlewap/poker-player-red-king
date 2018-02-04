77fb2e336829b0e361758a793442f8df1ce34ab8
Malyshkov Roman <malyshkov.roman@gmail.com>
2018-02-04 12:51:23 +0600

    All-in strategy.

--- player.py
+++ player.py
@@ -3,7 +3,10 @@ class Player:
     VERSION = "Red King"
 
     def betRequest(self, game_state):
-        return game_state['players'][game_state['in_action']]['stack']
+        try:
+            return game_state['players'][game_state['in_action']]['stack']
+        except BaseException:
+            return game_state['players'][game_state['in_action']]['stack']
 
     def showdown(self, game_state):
         pass
